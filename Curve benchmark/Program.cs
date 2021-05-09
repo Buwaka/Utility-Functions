e0221aa3f3bc5f51d18bf30ae4393a68cddb0001
BakaProxy <sammywyd@gmail.com>
2021-05-10 01:11:58 +0200

    cheapcurve benchmark

---
+++ Curve benchmark/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace ConsoleApp1
+{
+    public class BenchmarkTest
+    {
+
+        private readonly double time;
+
+        public BenchmarkTest()
+        {
+            time = new Random(69).NextDouble();
+            time *= new Random(45).Next();
+        }
+
+
+        // cheaper version of a cos() or sin() wave
+        [Benchmark]
+        public double CheapCurve()
+        {
+            //old
+            //per second interval plot (2x%2) * ((-2x)%2) * (1)^floor(x)
+            //cheaper version (per 2 seconds): plot (x%2) * ((-x)%2) * (1)^floor(x)
+            //c# % doesn't support inverse modulo, so here's a version without (per 1 second)
+            //(2x%2) * (2-(2x%2))
+
+            //float a = 2; // "speed", how fast we increment, simple terms the height
+            //with a = 2, we'll get a 0 to 1 loop no matter what, then b is just a time coefficient
+            //float b = Mathf.Pow((loopTime / a),-1); 
+            // a / b = time
+            // a/b = (b / a) ^ -1
+            //so (time / a) ^ -1
+
+
+            //new
+            //(x/t%1) * (1-x/t%1) * h * 4
+            //x(1−x) is symmetric around x=1/2, and that's where the peak is, so the peak height is 1/2 × 1/2 = 1/4.
+
+            double LoopTime = 1.0f;
+            double CurveHeight = 1.0f;
+            bool Negative = false;
+
+            double temp = (time / LoopTime % 1) * (1 - time / LoopTime % 1) * CurveHeight * 4;
+
+            if (Negative)
+            {
+                temp *= Math.Pow(-1, Math.Floor(time));
+            }
+
+            return temp;
+        }
+
+        [Benchmark]
+        public double CosCurve()
+        {
+            double LoopTime = 1.0f;
+            double CurveHeight = 1.0f;
+            bool Negative = false;
+
+
+            double temp = Math.Cos(time / (LoopTime / Math.PI)) * CurveHeight;
+
+            if (!Negative)
+            {
+                temp = Math.Abs(temp);
+            }
+
+            return temp;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var summary = BenchmarkRunner.Run<BenchmarkTest>();
+
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # Cheap Curve
 more efficient but less accurate sin/cos curve
 
+``` ini
+
+BenchmarkDotNet=v0.12.1, OS=Windows 10.0.19042
+Intel Core i5-8300H CPU 2.30GHz (Coffee Lake), 1 CPU, 8 logical and 4 physical cores
+  [Host]     : .NET Framework 4.8 (4.8.4341.0), X86 LegacyJIT  [AttachedDebugger]
+  DefaultJob : .NET Framework 4.8 (4.8.4341.0), X86 LegacyJIT
+
+
+```
+|     Method |     Mean |    Error |   StdDev |
+|----------- |---------:|---------:|---------:|
+| CheapCurve | 10.60 ns | 0.462 ns | 0.432 ns |
+|   CosCurve | 34.50 ns | 0.414 ns | 0.367 ns |
+
+
 # Sutils
     bool IsPointInRect(const Point2f& p, const Rectf& r);
     bool IsPointInCircle(const Point2f& p, const Circlef& c);
